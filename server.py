1336d0678992773c3d2d6e9fad3d7a5b95def03a
Rodion1198 <rodka1198@gmail.com>
2020-12-03 13:50:12 +0200

    return file content requirements.txt

--- server.py
+++ server.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/path/requirements')
+def file_content():
+    with open('requirements.txt', 'r') as f:
+        return render_template('index.html', text=f.read())
 
 
 if __name__ == '__main__':
