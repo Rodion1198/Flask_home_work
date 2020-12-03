2d782df8920245381db36a295195b24607e68a86
Rodion1198 <rodka1198@gmail.com>
2020-12-03 15:00:06 +0200

    generate 100 users+emails

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+from faker import Faker
 
 app = Flask(__name__)
 
@@ -12,6 +12,14 @@ def hello_world():
 def file_content():
     with open('requirements.txt', 'r') as f:
         return render_template('index.html', text=f.read())
+
+
+
+@app.route('/generate-users')
+def faker_content():
+    fake = Faker()
+    output = [fake.name() + fake.email() for i in range(100)]
+    return render_template('generate.html', title='Generate user', output=output)
 
 
 if __name__ == '__main__':
