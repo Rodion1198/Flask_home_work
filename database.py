4409edc9686a683cca6689c06eff1106de72b817
Rodion1198 <rodka1198@gmail.com>
2020-12-05 17:48:20 +0200

    added solution on amount of distinct names

--- database.py
+++ database.py
@@ -2,3 +2,42 @@ import os
 import sqlite3
 from faker import Faker
 
+
+fake = Faker()
+
+
+DEFAULT_PATH = os.path.join(os.path.dirname(__file__), 'db.sqlite3')
+
+
+def generate_user(count=0):
+    for _ in range(count):
+        name = fake.name()
+        yield name
+
+
+def init_database():
+    with sqlite3.connect(DEFAULT_PATH) as conn:
+        with conn as cursor:
+            cursor.execute(
+                """CREATE TABLE IF NOT EXISTS customers
+                (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL)"""
+            )
+            for user in generate_user(15):
+                cursor.execute(
+                    """INSERT INTO customers(name) VALUES (?)""",
+                    [user]
+                )
+
+
+# def exec_query(query, *args):
+#     with sqlite3.connect(DEFAULT_PATH) as conn:
+#         with conn as cursor:
+#             qs = cursor.execute(query, args)
+#             results = qs.fetchall()
+#     return results
+
+
+
+if __name__ == "__main__":
+    init_database()
